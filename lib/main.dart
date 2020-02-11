970d7c4597125b93c818cd3ad183bdae071a3c09
Noah Nakamura <noanakam@yahoo-corp.jp>
2020-02-11 12:00:09 +0900

    ring more bells

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,58 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  player.play('note1.wav');
+                },
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note2.wav');
+                },
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note3.wav');
+                },
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note4.wav');
+                },
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note5.wav');
+                },
+                color: Colors.teal,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note6.wav');
+                },
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note7.wav');
+                },
+                color: Colors.purple,
+              ),
+            ],
           ),
         ),
       ),
