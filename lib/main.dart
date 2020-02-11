a0eea5094a85b8762288926e8f0473a7219b1610
Noah Nakamura <noanakam@yahoo-corp.jp>
2020-02-11 12:45:28 +0900

    refactor widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,54 +6,53 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   final player = AudioCache();
 
+  Widget buildKey({Color color, String fileName}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          player.play(fileName);
+        },
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FlatButton(
-                onPressed: () {
-                  player.play('note1.wav');
-                },
+              buildKey(
                 color: Colors.red,
+                fileName: 'note1.wav',
               ),
-              FlatButton(
-                onPressed: () {
-                  player.play('note2.wav');
-                },
+              buildKey(
                 color: Colors.orange,
+                fileName: 'note2.wav',
               ),
-              FlatButton(
-                onPressed: () {
-                  player.play('note3.wav');
-                },
+              buildKey(
                 color: Colors.yellow,
+                fileName: 'note3.wav',
               ),
-              FlatButton(
-                onPressed: () {
-                  player.play('note4.wav');
-                },
+              buildKey(
                 color: Colors.green,
+                fileName: 'note4.wav',
               ),
-              FlatButton(
-                onPressed: () {
-                  player.play('note5.wav');
-                },
+              buildKey(
                 color: Colors.teal,
+                fileName: 'note5.wav',
               ),
-              FlatButton(
-                onPressed: () {
-                  player.play('note6.wav');
-                },
+              buildKey(
                 color: Colors.blue,
+                fileName: 'note6.wav',
               ),
-              FlatButton(
-                onPressed: () {
-                  player.play('note7.wav');
-                },
+              buildKey(
                 color: Colors.purple,
+                fileName: 'note7.wav',
               ),
             ],
           ),
